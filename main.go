b6592b066882a77b9fc46d0e545ab4a0b6308ced
Max Banister <max@maxbanister.com>
2025-03-30 02:10:01 -0700

    Add static web server

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"io/fs"
+	"log"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", HelloServer)
-	http.ListenAndServe(":8080", nil)
-}
+//go:embed public
+var staticFiles embed.FS
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+func main() {
+	var staticFS = fs.FS(staticFiles)
+	htmlContent, err := fs.Sub(staticFS, "public")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fs := http.FileServer(http.FS(htmlContent))
+
+	// Serve static files
+	http.Handle("/", fs)
+
+	port := "9000"
+
+	log.Printf("Listening on :%s...\n", port)
+	err = http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
