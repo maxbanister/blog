970f849836e414e34944ce274634a4c5bda7d007
Max Banister <max@maxbanister.com>
2025-04-03 02:01:52 -0700

    Add inbox

--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"bytes"
+	"crypto"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
 	"embed"
+	"encoding/base64"
 	"encoding/json"
+	"encoding/pem"
+	"fmt"
 	"io"
 	"io/fs"
 	"log"
 	"net/http"
 	"path/filepath"
+	"reflect"
 	"strings"
 )
 
@@ -28,7 +36,7 @@ func main() {
 	// Serve static files
 	mux.HandleFunc("/ap/users/@blog", redirectUser)
 	mux.HandleFunc("/ap/outbox", handleJSON)
-	mux.HandleFunc("/ap/inbox", handleInbox)
+	mux.HandleFunc("POST /ap/inbox", handleInbox)
 	mux.HandleFunc("/posts/", handleCondJSON)
 	mux.HandleFunc("/.well-known/webfinger", handleJSON)
 	mux.Handle("/", fs)
@@ -36,8 +44,37 @@ func main() {
 	log.Fatal(http.ListenAndServe(":9000", logHandler(mux)))
 }
 
+type HTTPLogWriter struct {
+	Writer     http.ResponseWriter
+	StatusCode int
+	ErrMsg     string
+}
+
+func NewHTTPLogWriter(w http.ResponseWriter) *HTTPLogWriter {
+	return &HTTPLogWriter{w, 0, ""}
+}
+func (w HTTPLogWriter) Header() http.Header {
+	return w.Writer.Header()
+}
+func (w *HTTPLogWriter) Write(resp []byte) (int, error) {
+	if w.StatusCode == 0 {
+		w.StatusCode = 200
+	} else {
+		w.ErrMsg = string(resp)
+	}
+	return w.Writer.Write(resp)
+}
+func (w *HTTPLogWriter) WriteHeader(statusCode int) {
+	w.StatusCode = statusCode
+	w.Writer.WriteHeader(statusCode)
+}
+
 func logHandler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/favicon.ico" {
+			h.ServeHTTP(w, r)
+			return
+		}
 		log.Println("Path:", r.URL.Path)
 		log.Println("Headers:", r.Header)
 		buf, _ := io.ReadAll(r.Body)
@@ -46,7 +83,17 @@ func logHandler(h http.Handler) http.Handler {
 		if len(buf) > 0 {
 			log.Println("Body:", string(buf))
 		}
-		h.ServeHTTP(w, r)
+		lw := NewHTTPLogWriter(w)
+		h.ServeHTTP(lw, r)
+		code := lw.StatusCode
+		fmt.Print("Response: ")
+		if code == 200 {
+			fmt.Println("200 OK")
+		} else if code == 404 || lw.ErrMsg == "" {
+			fmt.Printf("%d %s\n", code, http.StatusText(code))
+		} else {
+			fmt.Printf("%d %s - %s\n", code, http.StatusText(code), lw.ErrMsg)
+		}
 	})
 }
 
@@ -84,13 +131,146 @@ func acceptsJSON(vals []string) bool {
 }
 
 func handleInbox(w http.ResponseWriter, r *http.Request) {
-	jsonData := new(map[string]interface{})
-	err := json.NewDecoder(r.Body).Decode(&jsonData)
+	digests, ok := r.Header["Digest"]
+	if !ok || len(digests) == 0 {
+		http.Error(w, "no digest header", http.StatusBadRequest)
+		return
+	}
+	var digestFound bool
+	var digestBase64 string
+	for _, digest := range digests {
+		digestParts := strings.Split(digest, "=")
+		if len(digestParts) < 2 {
+			http.Error(w, "malformed digest header", http.StatusBadRequest)
+			return
+		}
+		if strings.ToLower(digestParts[0]) == "sha-256" {
+			digestBase64 = digestParts[1]
+			digestFound = true
+			break
+		}
+	}
+	if !digestFound {
+		http.Error(w, "unsupported digest algorithm", http.StatusBadRequest)
+		return
+	}
+	digestBytes, err := base64.StdEncoding.DecodeString(digestBase64)
+	if err != nil {
+		http.Error(w, "couldn't decode base64 digest: "+err.Error(),
+			http.StatusBadRequest)
+		return
+	}
+	followReqBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "couldn't read request body: "+err.Error(),
+			http.StatusBadRequest)
+		return
+	}
+	reqBodyHash := sha256.Sum256(followReqBody)
+	// I don't think this needs to be constant time...
+	if !bytes.Equal(reqBodyHash[:], digestBytes) {
+		http.Error(w, "digest did not match message body: "+err.Error(),
+			http.StatusBadRequest)
+		return
+	}
+
+	sigHeaders, ok := r.Header["Signature"]
+	if !ok {
+		http.Error(w, "no signature header", http.StatusBadRequest)
+		return
+	}
+	var sigBase64 string
+	for _, sig := range sigHeaders {
+		sigParts := strings.Split(sig, "=")
+		sigData := sigParts[1]
+		if strings.ToLower(sigParts[0]) == "signature" && len(sigData) > 1 {
+			// remove quotes
+			sigBase64 = sigData[1 : len(sigData)-1]
+		}
+	}
+	if sigBase64 == "" {
+		http.Error(w, "invalid signature", http.StatusBadRequest)
+		return
+	}
+	sigBytes, err := base64.StdEncoding.DecodeString(sigBase64)
+	if err != nil {
+		http.Error(w, "couldn't decode base64 signature: "+err.Error(),
+			http.StatusBadRequest)
+		return
+	}
+
+	requestJson := new(map[string]interface{})
+	err = json.NewDecoder(r.Body).Decode(&requestJson)
 	if err != nil {
 		http.Error(w, "bad json syntax: "+err.Error(), http.StatusBadRequest)
+		return
 	}
 	// fetch user object
-	// verify signature
+	if (*requestJson)["type"] != "Follow" {
+		http.Error(w, "unsupported operation", http.StatusNotImplemented)
+		return
+	}
+	actor, ok1 := (*requestJson)["actor"]
+	actorURL, ok2 := actor.(string)
+	if !ok1 || !ok2 {
+		http.Error(w, "no actor found", http.StatusBadRequest)
+		return
+	}
+
+	req, err := http.NewRequest("GET", actorURL, nil)
+	req.Header.Set("Accept", `application/ld+json; profile="https://www.w3.org/ns/activitystreams`)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	resp, err := (&http.Client{}).Do(req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	actorJson := new(map[string]interface{})
+	err = json.NewDecoder(resp.Body).Decode(&actorJson)
+	if err != nil {
+		http.Error(w, "bad json syntax: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+	publicKeyPEM, ok1 := (*actorJson)["publicKey"]
+	publicKeyBytes, ok2 := publicKeyPEM.([]byte)
+	if !ok1 || !ok2 {
+		http.Error(w, "no actor public key found", http.StatusBadRequest)
+		return
+	}
+	publicBlock, _ := pem.Decode(publicKeyBytes)
+	if publicBlock == nil || publicBlock.Type != "PUBLIC KEY" {
+		http.Error(w, "failed to decode public key", http.StatusBadRequest)
+		return
+	}
+	publicKey, err := x509.ParsePKIXPublicKey(publicBlock.Bytes)
+	if err != nil {
+		http.Error(w, "couldn't parse cert: "+err.Error(),
+			http.StatusBadRequest)
+		return
+	}
+	rsaPublicKey, ok := publicKey.(*rsa.PublicKey)
+	if !ok {
+		errMsg := fmt.Sprintf("invalid key type: %s", reflect.TypeOf(publicKey))
+		http.Error(w, errMsg, http.StatusBadRequest)
+		return
+	}
+	err = rsa.VerifyPSS(rsaPublicKey, crypto.SHA256, digestBytes, sigBytes, nil)
+	if err != nil {
+		http.Error(w, "signature did not match digest", http.StatusUnauthorized)
+		return
+	}
+
+	// verify signature:
+	// get http body
+	// hash http body
+	// check that the hash matches the digest
+	// decode pem into byte slice
+	// decode the signature using the byte array
+	// check that it makes the hash
+
 	// nothing will send a 200 OK
 	go AcceptRequest()
 }
