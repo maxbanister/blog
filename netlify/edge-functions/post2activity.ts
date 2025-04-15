43e5cb139720aa1c320e3206a73fccc66e70c7e6
Max Banister <max@maxbanister.com>
2025-04-15 04:27:30 -0700

    new edge function

---
+++ netlify/edge-functions/post2activity.ts
@@ -0,0 +1,28 @@
+import type { Config, Context } from "@netlify/edge-functions";
+import { outbox } from "../../public/ap/outbox";
+
+// This edge function takes a request for a post's URL with an Accept type of
+// activity+json and returns the item from the outbox that matches it.
+
+export default async (req: Request, context: Context) => {
+	// check if wants activity+json
+	let acceptHeader = req.headers.get("Accept") || "";
+	let wantsActivityJSON = acceptHeader.includes('application/activity+json');
+	if (!wantsActivityJSON)
+		// continue request chain by returning undefined
+		return;
+
+	// find the item corresponding to this url within the outbox
+	for (const post of outbox.ordereredItems) {
+		if (post.object.id == req.url) {
+			return new Response(JSON.stringify(post), {
+				headers: {"Content-Type": "application/activity+json"}
+			});
+		}
+	}
+};
+
+export const config: Config = {
+	path: "/posts",
+	onError: "bypass"
+};
