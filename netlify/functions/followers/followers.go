df31fd32d8c21f50ba5363d8441027f03554ccd7
Max Banister <max@maxbanister.com>
2025-04-15 23:57:31 -0700

    add followers function

---
+++ netlify/functions/followers/followers.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/maxbanister/blog/kv"
+	. "github.com/maxbanister/blog/util"
+	"google.golang.org/api/iterator"
+)
+
+func main() {
+	lambda.Start(handleFollowers)
+}
+
+func handleFollowers(request LambdaRequest) (*LambdaResponse, error) {
+	ctx := context.Background()
+	client, err := kv.GetFirestoreClient()
+	if err != nil {
+		fmt.Println("could not start firestore client:", err)
+		return &events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       err.Error(),
+		}, nil
+	}
+	defer client.Close()
+
+	var followers []string
+
+	iter := client.Collection("followers").Documents(ctx)
+	defer iter.Stop()
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			fmt.Println("could not call iter next on collection:", err)
+			return &events.APIGatewayProxyResponse{
+				StatusCode: http.StatusInternalServerError,
+				Body:       err.Error(),
+			}, nil
+		}
+		fmt.Println(doc.Data()["Id"])
+		followers = append(followers, doc.Data()["Id"].(string))
+	}
+
+	payloadStr := strings.Builder{}
+	payloadStr.WriteString(`{
+		"@context": "https://www.w3.org/ns/activitystreams",
+		"id": "https://maxscribes.netilfy.app/ap/followers",
+		"type": "OrderedCollection",
+		"totalItems": `)
+	payloadStr.WriteString(strconv.Itoa(len(followers)))
+	payloadStr.WriteString(`,
+		orderedItems: [`)
+	payloadStr.WriteString(strings.Join(followers, ","))
+	payloadStr.WriteString(`]
+		}`)
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       payloadStr.String(),
+	}, nil
+}
