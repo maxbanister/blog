5fdf4714a46826ebed47206842a5165547b9982f
Max Banister <max@maxbanister.com>
2025-05-08 01:39:03 -0700

    omg

--- public/js/replies.js
+++ public/js/replies.js
@@ -1,22 +1,51 @@
 (() => {
   // <stdin>
   var mastodonPrefix = "https://mastodon.social/authorize_interaction?uri=";
+  var recordsCache = /* @__PURE__ */ new Map();
+  async function getBlueskyURL(handle, postURL) {
+    console.log("looking for handle", handle);
+    let recordsJSON = recordsCache.get(handle);
+    if (!recordsJSON) {
+      bridgyEndpoint = "https://atproto.brid.gy/xrpc/com.atproto.repo.listRecords?repo=";
+      bridgyRequestURI = bridgyEndpoint + handle + "&collection=app.bsky.feed.post";
+      const resp = await fetch(bridgyRequestURI, {
+        headers: { "Accept": "application/json" }
+      });
+      recordsJSON = await resp.json();
+      recordsCache.set(handle, recordsJSON);
+      console.log("fetched record for", handle);
+    } else {
+      console.log("got record from cache for", handle);
+    }
+    if (!recordsJSON) {
+      console.log("could not get bluesky URL of post ", postURL);
+      return;
+    }
+    for (record of recordsJSON.records) {
+      if (record.value.bridgyOriginalUrl == postURL) {
+        const atURI = record.uri.replace("at://", "").replace("app.bsky.feed.post", "post");
+        return "https://bsky.app/profile/" + atURI;
+      }
+    }
+  }
   async function renderReplies() {
-    const mastodonAnchor = document.querySelector("#social-links > a");
+    const [mastodonAnchor, blueskyAnchor] = document.querySelectorAll("#social-links > a");
     mastodonAnchor.href = mastodonPrefix + window.location.href;
+    blueskyAnchor.href = await getBlueskyURL("maxbanister.com", "https://maxbanister.com" + window.location.pathname);
+    console.log(blueskyAnchor.href);
     const resp = await fetch(window.location.pathname + "replies");
     if (!resp.ok) {
       console.log(resp.statusText);
       return;
     }
     let repliesData = await resp.json();
-    console.log(repliesData);
     addRepliesRecursive(document.getElementById("replies"), repliesData.items);
   }
   function addRepliesRecursive(parentEl, replyItems) {
     if (!replyItems)
       return;
     for (item of replyItems) {
+      item.url = item.url.replace("https://fed.brid.gy/r/", "");
       const newReply = createAndAddReply(parentEl, {
         name: item.actor.name,
         shortName: item.actor.preferredUsername,
@@ -25,7 +54,7 @@
         userURL: item.actor.id,
         date: item.published,
         editDate: item.updated,
-        opURL: item.id,
+        opURL: item.url,
         content: item.content
       });
       addRepliesRecursive(newReply, item.replies.items);
@@ -69,11 +98,18 @@
     dateSpan.textContent = modifiedDate;
     const originalPostAnchor = clone.querySelector(".reply-op-button > a");
     originalPostAnchor.href = opURL;
-    const [mastodonReplyBtn, ...rest] = clone.querySelectorAll(".reply-controls a");
-    if (new URL(opURL).host == "mastodon.social") {
+    const [mastodonReplyBtn, blueskyReplyBtn] = clone.querySelectorAll(".reply-controls > a");
+    if (new URL(opURL).host === "mastodon.social") {
       mastodonReplyBtn.href = opURL;
     } else {
       mastodonReplyBtn.href = mastodonPrefix + opURL;
+    }
+    if (host === "bsky.brid.gy") {
+      blueskyReplyBtn.href = opURL;
+    } else {
+      getBlueskyURL(shortName + "." + host + ".ap.brid.gy", opURL).then((res) => {
+        blueskyReplyBtn.href = res;
+      });
     }
     parentEl.appendChild(clone);
     return cloneEl;
--- themes/default/assets/js/replies.js
+++ themes/default/assets/js/replies.js
@@ -1,8 +1,44 @@
 const mastodonPrefix = "https://mastodon.social/authorize_interaction?uri=";
 
+const recordsCache = new Map();
+
+async function getBlueskyURL(handle, postURL) {
+	console.log("looking for handle", handle);
+	let recordsJSON = recordsCache.get(handle);
+	if (!recordsJSON) {
+		bridgyEndpoint = "https://atproto.brid.gy/xrpc/com.atproto.repo.listRecords?repo=";
+		bridgyRequestURI = bridgyEndpoint + handle + "&collection=app.bsky.feed.post";
+
+		const resp = await fetch(bridgyRequestURI, {
+			headers: {"Accept": "application/json"}
+		});
+
+		recordsJSON = await resp.json();
+		recordsCache.set(handle, recordsJSON);
+		console.log("fetched record for", handle);
+	} else {
+		console.log("got record from cache for", handle);
+	}
+
+	//console.log(recordsJSON);
+	if (!recordsJSON) {
+		console.log("could not get bluesky URL of post ", postURL);
+		return;
+	}
+	for (record of recordsJSON.records) {
+		if (record.value.bridgyOriginalUrl == postURL) {
+			const atURI = record.uri.replace("at://", "")
+			                        .replace("app\.bsky\.feed\.post", "post");
+			return "https://bsky.app/profile/" + atURI;
+		}
+	}
+}
+
 async function renderReplies() {
-	const mastodonAnchor = document.querySelector("#social-links > a");
+	const [mastodonAnchor, blueskyAnchor] = document.querySelectorAll("#social-links > a");
 	mastodonAnchor.href = mastodonPrefix + window.location.href;
+	blueskyAnchor.href = await getBlueskyURL("maxbanister.com", "https://maxbanister.com" + window.location.pathname);
+	console.log(blueskyAnchor.href);
 
 	const resp = await fetch(window.location.pathname + "replies");
 	if (!resp.ok) {
@@ -10,7 +46,6 @@ async function renderReplies() {
 		return;
 	}
 	let repliesData = await resp.json();
-	console.log(repliesData);
 
 	addRepliesRecursive(document.getElementById("replies"), repliesData.items);
 }
@@ -20,6 +55,8 @@ function addRepliesRecursive(parentEl, replyItems) {
 		return;
 
 	for (item of replyItems) {
+		item.url = item.url.replace("https://fed.brid.gy/r/", "");
+
 		const newReply = createAndAddReply(parentEl, {
 			name: item.actor.name,
 			shortName: item.actor.preferredUsername,
@@ -76,12 +113,20 @@ function createAndAddReply(parentEl, params) {
 	const originalPostAnchor = clone.querySelector(".reply-op-button > a");
 	originalPostAnchor.href = opURL;
 
-	const [mastodonReplyBtn, ...rest] = clone.querySelectorAll(".reply-controls a");
-	if (new URL(opURL).host == "mastodon.social") {
+	const [mastodonReplyBtn, blueskyReplyBtn] = clone.querySelectorAll(".reply-controls > a");
+	if (new URL(opURL).host === "mastodon.social") {
 		mastodonReplyBtn.href = opURL;
 	}
 	else {
 		mastodonReplyBtn.href = mastodonPrefix + opURL;
+	}
+	if (host === "bsky.brid.gy") {
+		blueskyReplyBtn.href = opURL;
+	}
+	else {
+		getBlueskyURL(shortName + "." + host + ".ap.brid.gy", opURL).then((res) => {
+			blueskyReplyBtn.href = res;
+		});
 	}
 
 	parentEl.appendChild(clone);
