c244b8c5c6c73c59fb5d7e07aee40b3e59be6f4c
Max Banister <max@maxbanister.com>
2025-12-31 23:42:08 -0700

    Revert "remove public/"
    This reverts commit 3d68af1f5c62b46cdbcc5658acc5f88c0541bd44.

---
+++ public/js/interactions.js
@@ -0,0 +1,73 @@
+(() => {
+  // <stdin>
+  function colorHash(string) {
+    let hash = 0;
+    for (const char of string) {
+      hash = (hash << 5) - hash + char.charCodeAt(0);
+      hash |= 0;
+    }
+    const r = hash >> 0 & 255;
+    const g = hash >> 8 & 255;
+    const b = hash >> 16 & 255;
+    return "rgb(" + r + "," + g + "," + b + ",0.5)";
+  }
+  async function renderInteractions(typ) {
+    const likesOrSharesEl = document.getElementById(typ);
+    const labelEl = likesOrSharesEl.getElementsByTagName("label")[0];
+    const expandedEl = document.querySelector("#" + typ + " + .expanded");
+    const resp = await fetch(window.location.pathname + typ);
+    if (!resp.ok) {
+      console.log(resp.statusText);
+      return;
+    }
+    let items = await resp.json();
+    if (items.length == 0) {
+      return;
+    }
+    likesOrSharesEl.style.display = "flex";
+    expandedEl.style.display = "flex";
+    const typCapitalized = typ.charAt(0).toUpperCase() + typ.slice(1);
+    labelEl.firstChild.textContent = typCapitalized + " (" + items.length + ")";
+    previewImages = likesOrSharesEl.getElementsByClassName("preview_images")[0];
+    for (const [i, item] of items.entries()) {
+      const url = item.actor.id;
+      const actorHost = new URL(item.actor.id).hostname;
+      const actorName = item.actor.preferredUsername;
+      const handle = "@" + actorName + "@" + actorHost;
+      const imgSrc = "/image_proxy/" + encodeURIComponent(item.actor.icon) + "/" + typ + "/" + encodeURIComponent(item.id);
+      const aPreview = document.createElement("a");
+      aPreview.href = url;
+      aPreview.title = handle;
+      let img = document.createElement("img");
+      img.setAttribute("src", imgSrc);
+      img.setAttribute("alt", actorName[0]);
+      img.setAttribute("width", "32");
+      img.setAttribute("height", "32");
+      const r = actorName[0].charCodeAt(0);
+      const g = actorName[1].charCodeAt(0);
+      const b = actorName[2].charCodeAt(0);
+      img.style.backgroundColor = colorHash(handle);
+      aPreview.appendChild(img);
+      if (i <= 3) {
+        previewImages.appendChild(aPreview);
+      }
+      if (i == 3) {
+        previewImages.appendChild(document.createTextNode("\xA0\u2026"));
+      }
+      const a = document.createElement("a");
+      const span = document.createElement("span");
+      img = img.cloneNode();
+      a.href = url;
+      a.title = handle;
+      span.appendChild(img);
+      span.append(actorName);
+      span.style.display = "inline-block";
+      a.appendChild(span);
+      expandedEl.appendChild(a);
+    }
+  }
+  async function main() {
+    return Promise.all[renderInteractions("likes"), renderInteractions("shares")];
+  }
+  main();
+})();
---
+++ public/js/replies.js
@@ -0,0 +1,149 @@
+(() => {
+  // <stdin>
+  var mastodonPrefix = "https://mastodon.social/authorize_interaction?uri=";
+  function colorHash(string) {
+    let hash = 0;
+    for (const char of string) {
+      hash = (hash << 5) - hash + char.charCodeAt(0);
+      hash |= 0;
+    }
+    const r = hash >> 0 & 255;
+    const g = hash >> 8 & 255;
+    const b = hash >> 16 & 255;
+    return "rgb(" + r + "," + g + "," + b + ",0.5)";
+  }
+  async function getBlueskyURL(handle, cursor, postURL) {
+    console.log("fetching records for handle", handle + cursor);
+    bridgyRequestURI = "https://atproto.brid.gy/xrpc/com.atproto.repo.listRecords?repo=";
+    bridgyRequestURI += handle + "&collection=app.bsky.feed.post";
+    if (cursor) {
+      bridgyRequestURI += "&cursor=" + cursor;
+    }
+    const resp = await fetch(bridgyRequestURI, {
+      headers: { "Accept": "application/json" }
+    });
+    const recordsJSON = await resp.json();
+    console.log(recordsJSON);
+    if (!recordsJSON) {
+      console.log("could not get records for", handle + cursor);
+      return;
+    }
+    for (const record of recordsJSON.records) {
+      if (record.value.bridgyOriginalUrl == postURL) {
+        const atURI = record.uri.replace("at://", "").replace("app.bsky.feed.post", "post");
+        return "https://bsky.app/profile/" + atURI;
+      }
+    }
+    if (recordsJSON.cursor) {
+      return getBlueskyURL(handle, recordsJSON.cursor);
+    }
+  }
+  async function renderReplies() {
+    const [mastodonAnchor, blueskyAnchor] = document.querySelectorAll("#social-links > a");
+    mastodonAnchor.href = mastodonPrefix + window.location.href;
+    blueskyAnchor.href = await getBlueskyURL(
+      "maxbanister.com",
+      "",
+      "https://maxbanister.com" + window.location.pathname
+    );
+    const resp = await fetch(window.location.pathname + "replies");
+    if (!resp.ok) {
+      console.log(resp.statusText);
+      return;
+    }
+    let repliesData = await resp.json();
+    console.log(repliesData);
+    addRepliesRecursive(document.getElementById("replies"), repliesData.items);
+  }
+  function addRepliesRecursive(parentEl, replyItems) {
+    if (!replyItems)
+      return;
+    for (const item of replyItems) {
+      const deleted = item.type == "Tombstone";
+      item.url = deleted ? "javascript:void(0)" : item.url.replace("https://fed.brid.gy/r/", "");
+      item.actor ||= {};
+      const newReply = createAndAddReply(parentEl, {
+        id: item.id,
+        name: item.actor.name,
+        shortName: item.actor.preferredUsername,
+        host: deleted ? "" : new URL(item.actor.id).hostname,
+        picURL: item.actor.icon,
+        userURL: item.actor.id,
+        date: item.published,
+        editDate: item.updated,
+        opURL: item.url,
+        content: item.content
+      }, deleted);
+      addRepliesRecursive(newReply, item.replies.items);
+    }
+  }
+  function createAndAddReply(parentEl, params, deleted) {
+    const {
+      id,
+      name,
+      shortName,
+      host,
+      picURL,
+      userURL,
+      date,
+      editDate,
+      opURL,
+      content
+    } = params;
+    const options = {
+      dateStyle: "long",
+      timeStyle: "short"
+    };
+    let modifiedDate = new Intl.DateTimeFormat(void 0, options).format(new Date(date));
+    if (editDate) {
+      const dateEdited = new Intl.DateTimeFormat(void 0, options).format(new Date(editDate));
+      modifiedDate += " (Edited: " + dateEdited + ")";
+    }
+    const template = document.getElementById("reply-template");
+    const clone = template.content.cloneNode(true);
+    const cloneEl = clone.firstElementChild;
+    const [nameEl, hostEl] = clone.querySelectorAll(".reply-profile-info a span");
+    nameEl.textContent = deleted ? "" : "@" + shortName;
+    hostEl.textContent = deleted ? "" : "@" + host;
+    const contentEl = cloneEl.getElementsByClassName("reply-contents")[0];
+    contentEl.innerHTML = deleted ? '<i style="color: grey">[deleted]</i>' : content;
+    const profileImage = clone.querySelector(".reply-top > img");
+    profileImage.src = deleted ? "" : "/image_proxy/" + encodeURIComponent(picURL) + "/replies/" + encodeURIComponent(id);
+    profileImage.style.backgroundColor = colorHash(nameEl.innerText + hostEl.innerText);
+    if (deleted)
+      profileImage.alt = "";
+    const userAnchor = clone.querySelector(".reply-profile-info > a");
+    userAnchor.href = userURL;
+    const [nameSpan, dateSpan] = clone.querySelectorAll(".reply-profile-info > span");
+    nameSpan.textContent = deleted ? "[deleted]" : name ? name : shortName;
+    dateSpan.textContent = modifiedDate;
+    const originalPostAnchor = clone.querySelector(".reply-op-button > a");
+    originalPostAnchor.href = opURL;
+    const [mastodonReplyBtn, blueskyReplyBtn] = clone.querySelectorAll(".reply-controls > a");
+    if (new URL(opURL).host === "mastodon.social") {
+      mastodonReplyBtn.href = opURL;
+    } else {
+      mastodonReplyBtn.href = mastodonPrefix + id;
+    }
+    if (deleted || host === "bsky.brid.gy") {
+      blueskyReplyBtn.href = opURL;
+    } else {
+      const bridgedHandle = shortName + "." + host + ".ap.brid.gy";
+      blueskyReplyBtn.addEventListener("click", async (e) => {
+        console.log(blueskyReplyBtn.href);
+        if (blueskyReplyBtn.getAttribute("href") === "#") {
+          e.preventDefault();
+          const newLoc = await getBlueskyURL(bridgedHandle, "", opURL);
+          blueskyReplyBtn.href = newLoc;
+          blueskyReplyBtn.click();
+        }
+      });
+    }
+    parentEl.appendChild(clone);
+    return cloneEl;
+  }
+  async function main() {
+    return renderReplies();
+  }
+  main();
+})();
